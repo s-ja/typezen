e90ee3f6eb10e6036f37f7869f1ab6d72dcc3813
Keonsang Yun <kesay59@gmail.com>
2021-09-21 19:46:36 +0900

    feat : 텍스트 출력 및 오타 확인

--- src/pages/Type.vue
+++ src/pages/Type.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="absolute-center">
+        <div class="absolute-center bg-cusgrey" style="width: 700px">
             <template v-for="(chunk, idx) in chunkList" :key="idx">
                 <ChunkBlock :chunk="chunk"></ChunkBlock>
             </template>
@@ -8,7 +8,7 @@
     </div>
 </template>
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 import _ from 'lodash';
 
@@ -21,9 +21,36 @@ export default {
         ChunkBlock,
     },
     setup() {
-        const textFile = ref(TextFile.text);
-        const paragraphList = ref(_.split(textFile.value, '.'));
-        const chunkList = ref(_.chunk(paragraphList.value[0], 10));
+        const content = ref(TextFile.text);
+        const paragraphList = ref([]);
+        // _.split(textFile.value, '.')
+        const chunkList = ref([]);
+        // _.chunk(paragraphList.value[0], 50)
+
+        const makeParagraphList = function (content) {
+            paragraphList.value = _.split(content, '\n');
+        };
+
+        const makeChunkList = function (paragraph) {
+            let wordList = _.split(paragraph, ' ');
+            let chunk = '';
+            wordList.forEach((word) => {
+                if (chunk.length + word.length + 4 > 60) {
+                    chunkList.value.push(chunk.substr(1));
+                    chunk = '';
+                }
+                chunk = chunk.concat(' '.concat(word));
+            });
+            chunkList.value.push(chunk.substr(1));
+        };
+
+        onMounted(() => {
+            makeParagraphList(content.value);
+            paragraphList.value.forEach((paragraph) => {
+                makeChunkList(paragraph);
+            });
+            // makeChunkList('가나다 라마바 사아자');
+        });
 
         return {
             chunkList,
