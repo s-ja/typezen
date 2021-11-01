0f06021ccbef9ec0dbf95c2cf6299b9298ade6bf
Keonsang Yun <kesay59@gmail.com>
2021-11-01 23:54:29 +0900

    feat : 라우터 링크

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,37 @@
 <template>
     <q-page class="flex flex-center">
-        <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px" />
+        <!-- <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px" /> -->
+        <div>
+            <div class="menu-select" @click="handleClickType">Type</div>
+            <div class="menu-select">Upload</div>
+        </div>
     </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
     name: 'PageIndex',
+    setup() {
+        const $router = useRouter();
+
+        const handleClickType = function () {
+            $router.push('/write');
+        };
+
+        return {
+            handleClickType,
+        };
+    },
 });
 </script>
+
+<style lang="sass" scoped>
+.menu-select
+    font-size: 20px
+    color: white
+.menu-select:hover
+    color: red
+</style>
