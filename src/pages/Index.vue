ab89facc5802d0c3b7c190528bd0a2d738b2a841
Keonsang Yun <kesay59@gmail.com>
2021-11-16 23:58:14 +0900

    feat : dialog 추가

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,6 +9,22 @@
             </div>
         </div>
     </q-page>
+    <q-dialog v-model="uploadDialog" persistent>
+        <q-card style="min-width: 350px">
+            <q-card-section>
+                <div class="text-h6">Your address</div>
+            </q-card-section>
+
+            <!-- <q-card-section class="q-pt-none">
+                <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" />
+            </q-card-section> -->
+
+            <q-card-actions align="right" class="text-primary">
+                <q-btn flat label="Cancel" v-close-popup />
+                <q-btn flat label="Add address" v-close-popup />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
 </template>
 
 <script>
@@ -30,8 +46,11 @@ export default defineComponent({
 
         // }
 
+        const uploadDialog = ref(false);
+
         return {
             handleClickType,
+            uploadDialog,
         };
     },
 });
